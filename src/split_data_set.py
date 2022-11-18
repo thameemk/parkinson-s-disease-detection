ccf72fa2e2e2f0e59fb0dd6f1f5c1b417be9934f
thameemk612 <thameemk612@gmail.com>
2022-11-19 00:13:44 +0530

    Split Data Set / Data Modelling

---
+++ src/split_data_set.py
@@ -0,0 +1,14 @@
+#  Project : Detection of Parkinson's Disease Using Vocal Features: An Eigen Approach
+#  Filename : split_data_set.py
+#  Author : thameem
+#  Modified time : Sat, 19 Nov 2022 at 12:08 am India Standard Time
+
+def split_train_and_test_data(x, y):
+    """
+    split the given data set into training & testing datas
+
+    """
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+    return x_train, x_test, y_train, y_test
