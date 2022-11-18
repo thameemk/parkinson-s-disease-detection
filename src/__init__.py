607bba6c526be18e5cad8f3be76f442a505dd83d
thameemk612 <thameemk612@gmail.com>
2022-11-19 00:01:31 +0530

    Load Data Set

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,5 @@
 #  Filename : __init__.py
 #  Author : thameem
 #  Modified time : Fri, 18 Nov 2022 at 11:04 pm India Standard Time
+
+from .load_data_set import load_data_set
---
+++ src/load_data_set.py
@@ -0,0 +1,20 @@
+#  Project : Detection of Parkinson's Disease Using Vocal Features: An Eigen Approach
+#  Filename : load_data_set.py
+#  Author : thameem
+#  Modified time : Fri, 18 Nov 2022 at 11:30 pm India Standard Time
+import pandas as pd
+from numpy import ndarray
+
+
+def load_data_set() -> [ndarray, ndarray]:
+    """
+    helps to load the data set
+
+    Returns: data set
+    """
+    dataset = pd.read_csv('../data/parkinsons.data')
+
+    x = dataset.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 18, 19, 20, 21, 22, 23]].values
+    y = dataset.iloc[:, 17].values
+
+    return x, y
