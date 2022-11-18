273fb139aa8a846daf665af51a0d0f62974b9cfe
thameemk612 <thameemk612@gmail.com>
2022-11-18 23:09:24 +0530

    Add: gitignore, src

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+#  Project : Detection of Parkinson's Disease Using Vocal Features: An Eigen Approach
+#  Filename : __init__.py
+#  Author : thameem
+#  Modified time : Fri, 18 Nov 2022 at 11:04 pm India Standard Time
