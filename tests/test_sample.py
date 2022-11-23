04044051b844bf10a38567b9bdcf3461e90e1464
thameemk612 <thameemk612@gmail.com>
2022-11-24 00:27:28 +0530

    Add: test sample

---
+++ tests/test_sample.py
@@ -0,0 +1,7 @@
+#  Project : Detection of Parkinson's Disease Using Vocal Features: An Eigen Approach
+#  Filename : test_sample.py
+#  Author : thameem
+#  Modified time : Thu, 24 Nov 2022 at 12:27 am India Standard Time
+
+def test_sample():
+    pass
